06c82ce47dc992e7f2f6a2016e1e71e3546ed1d1
liuzhiqiang <1964252235@qq.com>
2020-03-15 22:07:56 +0800

    fix

--- routers/routers.go
+++ routers/routers.go
@@ -22,6 +22,6 @@ func StartHttpServer(port int) {
 
 	}
 	//err := router.Run(fmt.Sprintf("%s:%d"),"0.0.0.0", port)
-	err := router.Run(fmt.Sprintf("%s:%d", "localhost", port))
+	err := router.Run(fmt.Sprintf("%s:%d", "0.0.0.0", port))
 	fmt.Println(err)
 }
